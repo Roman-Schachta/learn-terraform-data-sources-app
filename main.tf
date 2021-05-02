7d0bda7cba4aef4fcaa2eda00444d1df1afa09ee
Roman-Schachta <71693811+Roman-Schachta@users.noreply.github.com>
2021-05-02 22:40:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+
+  config = {
+    path = "../learn-terraform-data-sources-vpc/terraform.tfstate"
+  }
+}
+
 terraform {
   required_providers {
     aws = {
@@ -7,7 +15,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = data.terraform_remote_state.vpc.outputs.aws_region
 }
 
 resource "random_string" "lb_id" {
@@ -24,8 +32,9 @@ module "elb_http" {
 
   internal = false
 
-  security_groups = []
-  subnets         = []
+  security_groups = data.terraform_remote_state.vpc.outputs.lb_security_group_ids
+  subnets         = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+
 
   number_of_instances = length(aws_instance.app)
   instances           = aws_instance.app.*.id
@@ -47,12 +56,12 @@ module "elb_http" {
 }
 
 resource "aws_instance" "app" {
-  ami = "ami-04d29b6f966df1537"
-
+  ami = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
-  subnet_id              = ""
-  vpc_security_group_ids = []
+  subnet_id              = data.terraform_remote_state.vpc.outputs.private_subnet_ids[count.index % length(data.terraform_remote_state.vpc.outputs.private_subnet_ids)]
+  vpc_security_group_ids = data.terraform_remote_state.vpc.outputs.app_security_group_ids
+
 
   user_data = <<-EOF
     #!/bin/bash
